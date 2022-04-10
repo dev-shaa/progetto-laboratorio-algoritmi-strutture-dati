d6803d22b2dd719264b1d8bb157823ca125c8078
dev-shaa <salvatoredg.s@libero.it>
2022-04-10 10:19:39 +0200

    replaced templates with developed files

--- exercise2/container/container.cpp
+++ exercise2/container/container.cpp
@@ -1,10 +1,141 @@
+namespace lasd
+{
 
-namespace lasd {
+    /* ************************************************************************** */
 
-/* ************************************************************************** */
+    inline bool Container::Empty() const noexcept
+    {
+        return size == 0;
+    }
 
-// ...
+    inline ulong Container::Size() const noexcept
+    {
+        return size;
+    }
 
-/* ************************************************************************** */
+    /* ************************************************************************** */
+
+    template <typename Data>
+    bool LinearContainer<Data>::operator==(const LinearContainer &other) const noexcept
+    {
+        if (size != other.size)
+            return false;
+
+        ulong i = 0;
+        bool equals = true;
+
+        while (i < size && equals)
+        {
+            equals = (*this)[i] == other[i];
+            i++;
+        }
+
+        return equals;
+    }
+
+    template <typename Data>
+    bool LinearContainer<Data>::operator!=(const LinearContainer &other) const noexcept
+    {
+        return !(*this == other);
+    }
+
+    template <typename Data>
+    Data &LinearContainer<Data>::Front() const
+    {
+        if (Empty())
+            throw std::length_error("Can't access front item because container is empty");
+
+        return (*this)[0];
+    }
+
+    template <typename Data>
+    Data &LinearContainer<Data>::Back() const
+    {
+        if (Empty())
+            throw std::length_error("Can't access back item because container is empty");
+
+        return (*this)[size - 1];
+    }
+
+    /* ************************************************************************** */
+
+    template <typename Data>
+    void setFlagIfEqual(const Data &value, const void *otherData, void *flag)
+    {
+        if (value == *((Data *)otherData))
+            *((bool *)flag) = true;
+    }
+
+    template <typename Data>
+    bool FoldableContainer<Data>::Exists(const Data &value) const noexcept
+    {
+        bool exists = false;
+        Fold(&setFlagIfEqual<Data>, &value, &exists);
+        return exists;
+    }
+
+    /* ************************************************************************** */
+
+    template <typename Data>
+    void PreOrderMappableContainer<Data>::Map(MapFunctor functor, void *par)
+    {
+        MapPreOrder(functor, par);
+    }
+
+    /* ************************************************************************** */
+
+    template <typename Data>
+    void PreOrderFoldableContainer<Data>::Fold(FoldFunctor functor, const void *par, void *accumulator) const
+    {
+        FoldPreOrder(functor, par, accumulator);
+    }
+
+    /* ************************************************************************** */
+
+    template <typename Data>
+    void PostOrderMappableContainer<Data>::Map(MapFunctor functor, void *par)
+    {
+        MapPostOrder(functor, par);
+    }
+
+    /* ************************************************************************** */
+
+    template <typename Data>
+    void PostOrderFoldableContainer<Data>::Fold(FoldFunctor functor, const void *par, void *accumulator) const
+    {
+        FoldPostOrder(functor, par, accumulator);
+    }
+
+    /* ************************************************************************** */
+
+    template <typename Data>
+    void InOrderMappableContainer<Data>::Map(MapFunctor functor, void *par)
+    {
+        MapInOrder(functor, par);
+    }
+
+    /* ************************************************************************** */
+
+    template <typename Data>
+    void InOrderFoldableContainer<Data>::Fold(FoldFunctor functor, const void *par, void *accumulator) const
+    {
+        FoldInOrder(functor, par, accumulator);
+    }
+
+    /* ************************************************************************** */
+
+    template <typename Data>
+    void BreadthMappableContainer<Data>::Map(MapFunctor functor, void *par)
+    {
+        MapBreadth(functor, par);
+    }
+
+    /* ************************************************************************** */
+
+    template <typename Data>
+    void BreadthFoldableContainer<Data>::Fold(FoldFunctor functor, const void *par, void *accumulator) const
+    {
+        FoldBreadth(functor, par, accumulator);
+    }
 
 }
--- exercise2/list/list.hpp
+++ exercise2/list/list.hpp
@@ -1,4 +1,3 @@
-
 #ifndef LIST_HPP
 #define LIST_HPP
 
@@ -8,185 +7,107 @@
 
 /* ************************************************************************** */
 
-namespace lasd {
+namespace lasd
+{
 
-/* ************************************************************************** */
+  /* ************************************************************************** */
 
-template <typename Data>
-class List {
-              // Must extend LinearContainer<Data>,
-              //             PreOrderMappableContainer<Data>,
-              //             PostOrderMappableContainer<Data>,
-              //             PreOrderFoldableContainer<Data>,
-              //             PostOrderFoldableContainer<Data>
+  template <typename Data>
+  class List : virtual public LinearContainer<Data>,
+               virtual public PreOrderMappableContainer<Data>,
+               virtual public PostOrderMappableContainer<Data>,
+               virtual public PreOrderFoldableContainer<Data>,
+               virtual public PostOrderFoldableContainer<Data>
+  {
+  protected:
+    struct Node
+    {
+      Data value;
+      Node *next = nullptr;
 
-private:
+      /* ********************************************************************** */
 
-  // ...
+      Node() = default;
+      Node(const Data &value);
+      Node(Data &&value) noexcept;
 
-protected:
+      Node(const Node &node);
+      Node(Node &&node) noexcept;
 
-  // using LinearContainer<Data>::???;
+      virtual ~Node() = default;
 
-  struct Node {
+      /* ********************************************************************** */
 
-    // Data
-    // ...
+      Node &operator=(const Node &other);
+      Node &operator=(Node &&other) noexcept;
 
-    /* ********************************************************************** */
+      bool operator==(const Node &other) const noexcept;
+      bool operator!=(const Node &other) const noexcept;
+    };
 
-    // Specific constructors
-    // ...
+    using LinearContainer<Data>::size;
 
-    /* ********************************************************************** */
+    Node *start = nullptr;
+    Node *end = nullptr;
 
-    // Copy constructor
-    // ...
+  public:
+    List() = default;
+    List(const LinearContainer<Data> &linearContainer);
 
-    // Move constructor
-    // ...
+    List(const List &list);
+    List(List &&list) noexcept;
 
-    /* ********************************************************************** */
+    virtual ~List();
 
-    // Destructor
-    // ...
+    /* ************************************************************************ */
 
-    /* ********************************************************************** */
+    List &operator=(const List &other);
+    List &operator=(List &&other) noexcept;
 
-    // Comparison operators
-    // ...
+    bool operator==(const List &other) const noexcept;
+    bool operator!=(const List &other) const noexcept;
 
-    /* ********************************************************************** */
+    Data &operator[](const ulong index) const override;
 
-    // Specific member functions
+    /* ************************************************************************ */
 
-    // ...
+    void InsertAtFront(const Data &item);
+    void InsertAtFront(Data &&item) noexcept;
+    void RemoveFromFront();
+    Data &FrontNRemove();
 
+    void InsertAtBack(const Data &item);
+    void InsertAtBack(Data &&item) noexcept;
+
+    /* ************************************************************************ */
+
+    Data &Front() const override;
+    Data &Back() const override;
+
+    void Clear() override;
+
+    /* ************************************************************************ */
+
+    using typename MappableContainer<Data>::MapFunctor;
+
+    void Map(MapFunctor, void *) override;
+    void MapPreOrder(MapFunctor functor, void *) override;
+    void MapPostOrder(MapFunctor functor, void *) override;
+
+    /* ************************************************************************ */
+
+    using typename FoldableContainer<Data>::FoldFunctor;
+
+    void Fold(FoldFunctor functor, const void *, void *accumulator) const override;
+    void FoldPreOrder(FoldFunctor functor, const void *, void *accumulator) const override;
+    void FoldPostOrder(FoldFunctor functor, const void *, void *accumulator) const override;
+
+  protected:
+    void AuxMapPostOrder(MapFunctor functor, void *par, Node *node);
+    void AuxFoldPostOrder(FoldFunctor functor, const void *par, void *accumulator, Node *node) const;
   };
 
-  // ...
-
-public:
-
-  // Default constructor
-  // List() specifiers;
-
-  /* ************************************************************************ */
-
-  // Specific constructor
-  // List(argument) specifiers; // A list obtained from a LinearContainer
-
-  /* ************************************************************************ */
-
-  // Copy constructor
-  // List(argument) specifiers;
-
-  // Move constructor
-  // List(argument) specifiers;
-
-  /* ************************************************************************ */
-
-  // Destructor
-  // ~List() specifiers;
-
-  /* ************************************************************************ */
-
-  // Copy assignment
-  // type operator=(argument) specifiers;
-
-  // Move assignment
-  // type operator=(argument) specifiers;
-
-  /* ************************************************************************ */
-
-  // Comparison operators
-  // type operator==(argument) specifiers;
-  // type operator!=(argument) specifiers;
-
-  /* ************************************************************************ */
-
-  // Specific member functions
-
-  // type InsertAtFront(argument) specifier; // Copy of the value
-  // type InsertAtFront(argument) specifier; // Move of the value
-  // type RemoveFromFront() specifier; // (must throw std::length_error when empty)
-  // type FrontNRemove() specifier; // (must throw std::length_error when empty)
-
-  // type InsertAtBack(argument) specifier; // Copy of the value
-  // type InsertAtBack(argument) specifier; // Move of the value
-
-  /* ************************************************************************ */
-
-  // Specific member functions (inherited from Container)
-
-  // type Clear() specifiers; // Override Container member
-
-  /* ************************************************************************ */
-
-  // Specific member functions (inherited from LinearContainer)
-
-  // type Front() specifiers; // Override LinearContainer member (must throw std::length_error when empty)
-  // type Back() specifiers; // Override LinearContainer member (must throw std::length_error when empty)
-
-  // type operator[](argument) specifiers; // Override LinearContainer member (must throw std::out_of_range when out of range)
-
-  /* ************************************************************************ */
-
-  // Specific member functions (inherited from MappableContainer)
-
-  // using typename MappableContainer<Data>::MapFunctor;
-
-  // type Map(arguments) specifiers; // Override MappableContainer member
-
-  /* ************************************************************************ */
-
-  // Specific member functions (inherited from PreOrderMappableContainer)
-
-  // type MapPreOrder(arguments) specifiers; // Override PreOrderMappableContainer member
-
-  /* ************************************************************************ */
-
-  // Specific member functions (inherited from PostOrderMappableContainer)
-
-  // type MapPostOrder(arguments) specifiers; // Override PostOrderMappableContainer member
-
-  /* ************************************************************************ */
-
-  // Specific member functions (inherited from FoldableContainer)
-
-  // using typename FoldableContainer<Data>::FoldFunctor;
-
-  // type Fold(arguments) specifiers; // Override FoldableContainer member
-
-  /* ************************************************************************ */
-
-  // Specific member functions (inherited from PreOrderFoldableContainer)
-
-  // type FoldPreOrder(arguments) specifiers; // Override FoldableContainer member
-
-  /* ************************************************************************ */
-
-  // Specific member functions (inherited from PostOrderFoldableContainer)
-
-  // type FoldPostOrder(arguments) specifiers; // Override FoldableContainer member
-
-protected:
-
-  // Auxiliary member functions (for PreOrderMappableContainer & PostOrderMappableContainer)
-
-  // type MapPreOrder(arguments) specifiers; // Accessory function executing from one point of the list onwards
-  // type MapPostOrder(arguments) specifiers; // Accessory function executing from one point of the list onwards
-
-  /* ************************************************************************ */
-
-  // Auxiliary member functions (for PreOrderFoldableContainer & PostOrderFoldableContainer)
-
-  // type FoldPreOrder(arguments) specifiers; // Accessory function executing from one point of the list onwards
-  // type FoldPostOrder(arguments) specifiers; // Accessory function executing from one point of the list onwards
-
-};
-
-/* ************************************************************************** */
+  /* ************************************************************************** */
 
 }
 
