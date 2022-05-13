d68a57a238b70111a51929712fa8cc81257c641c
Salvatore <salvatore.digennaro@outlook.it>
2022-05-13 20:58:39 +0200

    Exists implementation

--- exercise4/bst/bst.cpp
+++ exercise4/bst/bst.cpp
@@ -1,10 +1,20 @@
 
-namespace lasd {
+namespace lasd
+{
 
-/* ************************************************************************** */
+    /* ************************************************************************** */
 
-// ...
+    template <typename Data>
+    bool BST::Exists(const Data &value) const noexcept
+    {
+        NodeLnk *node = root;
 
-/* ************************************************************************** */
+        while (node != nullptr && node->Element() != value)
+            node = value < node->Element() ? node->leftChild : node->rightChild;
+
+        return node != nullptr;
+    }
+
+    /* ************************************************************************** */
 
 }
