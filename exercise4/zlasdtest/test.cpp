127e9e4859a3e5b76342706b63dc2b347fb78b06
dev-shaa <salvatore.digennaro@outlook.it>
2022-05-16 12:12:17 +0200

    template fix

--- exercise4/zlasdtest/test.cpp
+++ exercise4/zlasdtest/test.cpp
@@ -5,7 +5,7 @@
 
 #include "./exercise3/test.hpp"
 
-// #include "./exercise4/test.hpp"
+#include "./exercise4/test.hpp"
 
 // #include "./exercise5/test.hpp"
 
@@ -17,20 +17,17 @@ using namespace std;
 
 /* ************************************************************************** */
 
-void lasdtest()
-{
-  cout << endl
-       << "~*~#~*~ Welcome to the LASD Test Suite ~*~#~*~ " << endl;
+void lasdtest() {
+  cout << endl << "~*~#~*~ Welcome to the LASD Test Suite ~*~#~*~ " << endl;
   testSimpleExercise1();
   testFullExercise1();
   testSimpleExercise2();
   testFullExercise2();
   testSimpleExercise3();
   testFullExercise3();
-  // testSimpleExercise4();
-  // testFullExercise4();
+  testSimpleExercise4();
+  testFullExercise4();
   // testSimpleExercise5();
   // testFullExercise5();
-  cout << endl
-       << "Goodbye!" << endl;
+  cout << endl << "Goodbye!" << endl;
 }
